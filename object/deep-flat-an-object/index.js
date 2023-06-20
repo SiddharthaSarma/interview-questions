73f982475e22801521f590217deac26e3ca79b61
Siddhartha <sidduinevitable@gmail.com>
2023-06-20 21:48:50 +0530

    solution for deep flat an array

---
+++ object/deep-flat-an-object/index.js
@@ -0,0 +1,43 @@
+const obj = {
+  A: '1',
+  B: null,
+  C: {
+    D: {
+      E: 'test'
+    },
+    F: {
+      G: {
+        J: 'hello',
+        K: 'abcd'
+      }
+    }
+  },
+  H: {
+    I: [10, 20]
+  }
+};
+
+/*
+  expected output
+  A: '1',
+  B: null,
+  C.D.E : 'test',
+  C.F.G: 'hello',
+  H.G.0: 10,
+  H.G.1: 20
+*/
+
+function solution(input, prefix='') {
+  let result = {};
+  for (const k in input) {
+    const val = input[k];
+    if (val && typeof val === 'object') {
+      result = {...result, ...solution(input[k], k + '.' + prefix)};
+    } else {
+      result[prefix + k] = val;
+    }
+  }
+  return result;
+}
+
+console.log(solution(obj));
